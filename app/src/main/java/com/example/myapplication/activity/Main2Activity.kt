a91bff9412060d174cb37a103606ee508200a6b6
王国泰 <wangguotai@xiaomi.com>
2024-03-01 10:19:26 +0800

    feat(viewPager): step2
    Signed-off-by: 王国泰 <wangguotai@xiaomi.com>

---
+++ app/src/main/java/com/example/myapplication/activity/Main2Activity.kt
@@ -0,0 +1,12 @@
+package com.example.myapplication.activity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.myapplication.R
+
+class Main2Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_2)
+    }
+}

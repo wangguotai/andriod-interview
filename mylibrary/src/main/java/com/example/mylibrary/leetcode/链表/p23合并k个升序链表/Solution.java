1385bcc41a0bd1128389067c58b259b0024b99a2
王国泰 <wangguotai@xiaomi.com>
2024-03-07 14:40:05 +0800

    feat: p23
    Signed-off-by: 王国泰 <wangguotai@xiaomi.com>

---
+++ mylibrary/src/main/java/com/example/mylibrary/leetcode/链表/p23合并k个升序链表/Solution.java
@@ -0,0 +1,37 @@
+package com.example.mylibrary.leetcode.链表.p23合并k个升序链表;
+
+import com.example.mylibrary.leetcode.链表.ListNode;
+
+public class Solution {
+    // 类似归并排序，将列表两两合并之后合并为一个
+    public ListNode mergeKLists(ListNode[] lists) {
+        return mergeKLists(lists, 0, lists.length - 1);
+    }
+
+    public ListNode mergeKLists(ListNode[] lists, int start, int end) {
+        if (start == end) {
+            return lists[start];
+        }
+        int mid = (start + end) / 2;
+        ListNode left = mergeKLists(lists, start, mid);
+        ListNode right = mergeKLists(lists, mid + 1, end);
+        return merge(left, right);
+    }
+
+    private ListNode merge(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode();
+        ListNode curr = dummy;
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                curr.next = l1;
+                l1 = l1.next;
+            } else {
+                curr.next = l2;
+                l2 = l2.next;
+            }
+            curr = curr.next;
+        }
+        curr.next = l1 == null ? l2 : l1;
+        return dummy.next;
+    }
+}

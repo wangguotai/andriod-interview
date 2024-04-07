057efb77efb75ba2ad79f01bfe59f6568888def0
王国泰 <wangguotai@xiaomi.com>
2024-04-07 14:48:13 +0800

    feat(xshell): 进入Release
    Signed-off-by: 王国泰 <wangguotai@xiaomi.com>

--- xshell/login/src/main/java/com/mi/login/LoginMainActivity.kt
+++ xshell/login/src/main/java/com/mi/login/LoginMainActivity.kt
@@ -2,10 +2,9 @@ package com.mi.login
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-
 class LoginMainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_main_login)
     }
 }

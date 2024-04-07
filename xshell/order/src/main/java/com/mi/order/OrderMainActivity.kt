6f38c7173fb165c38e18e7851d70e8fc978287c9
王国泰 <wangguotai@xiaomi.com>
2024-04-07 15:37:31 +0800

    feat(xshell-order模块): 增添主Activity 和 测试+线上的Manifest
    Signed-off-by: 王国泰 <wangguotai@xiaomi.com>

---
+++ xshell/order/src/main/java/com/mi/order/OrderMainActivity.kt
@@ -0,0 +1,11 @@
+package com.mi.order
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class OrderMainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_order_main)
+    }
+}

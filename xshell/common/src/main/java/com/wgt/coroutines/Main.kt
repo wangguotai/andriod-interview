d4d69993286908a6393fd7662280aefc53ace245
王国泰 <wangguotai@xiaomi.com>
2024-04-16 15:39:56 +0800

    feat(RN):  创建RN demo
    Signed-off-by: 王国泰 <wangguotai@xiaomi.com>

--- xshell/common/src/main/java/com/wgt/coroutines/Main.kt
+++ xshell/common/src/main/java/com/wgt/coroutines/Main.kt
@@ -1,34 +1,55 @@
 package com.wgt.coroutines
 
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.asCoroutineDispatcher
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
+import java.util.concurrent.Executors
 
 //fun main(){
 
 //}
+suspend fun suspendPrint() {
+    withContext(Dispatchers.IO) {
+        println("Thread:${Thread.currentThread().name}")
+    }
+}
 
 fun main() {
-    suspend fun suspendPrint() {
-        withContext(Dispatchers.IO) {
-            println("Thread:${Thread.currentThread().name}")
-        }
-    }
+
 //        val coroutineScope = CoroutineScope(Dispatchers.Main)
 //    GlobalScope.launch {
 //        suspendPrint()
 //        println("After suspend")
 //    }
-    GlobalScope.launch {
-        println("Coroutine started: ${Thread.currentThread().name}")
-        suspendPrint()
+    var i = 0
+    repeat(10) {
+        CoroutineScope(Executors.newFixedThreadPool(1).asCoroutineDispatcher())
+            .launch {
+                println("Coroutine started: ${Thread.currentThread().name} ${i++}")
+                suspendPrint()
 //        delay(1000L)
-        println("Hello World!")
+//            println("Hello World!")
+            }
     }
     println("In Main")
 
 
     println("After launch:${Thread.currentThread().name}")
-    Thread.sleep(2000L)
+    Thread.sleep(12000L)
 }
+//
+//fun main(){
+//    Thread{
+//        var i = 0
+//        repeat(100, {
+//            val job = GlobalScope.launch {
+//                println("Coroutine started on thread:${Thread.currentThread()}, ${i++}")
+//            }
+////            runBlocking {
+////                job.join()
+////            }
+//        })
+//    }
+//}
---
+++ xshell/rnModule/readme.md
@@ -0,0 +1,4 @@
+### 创建一个RN模块并注册到已有的项目模块中使用
+
+1. npm init  (创建package.json)
+2. npm add react-native

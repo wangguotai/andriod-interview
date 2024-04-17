cb2e15387826a7c6a9d8972b4e84608410070238
王国泰 <wangguotai@xiaomi.com>
2024-04-17 09:42:21 +0800

    feat(leet): dp p95
    Signed-off-by: 王国泰 <wangguotai@xiaomi.com>

--- mylibrary/src/main/java/com/example/mylibrary/leetcode/动态规划/p95最长公共子序列/Solution.java
+++ mylibrary/src/main/java/com/example/mylibrary/leetcode/动态规划/p95最长公共子序列/Solution.java
@@ -1,5 +1,23 @@
 package com.example.mylibrary.leetcode.动态规划.p95最长公共子序列;
 
 public class Solution {
+    public int longestCommonSubSequence(String text1, String text2) {
+        if (text1 == null || text2 == null) {
+            return 0;
+        }
+        int m = text1.length();
+        int n = text2.length();
+        int dp[][] = new int[m][n];
 
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (text1.charAt(i - 1) == text2.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+        return dp[m][n];
+    }
 }

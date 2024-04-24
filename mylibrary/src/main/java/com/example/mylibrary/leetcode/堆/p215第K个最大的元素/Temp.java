ec8f63e6fcc781855728e559b8d40f3d4005f330
王国泰 <wangguotai@xiaomi.com>
2024-04-24 16:36:43 +0800

    feat: leetcode
    Signed-off-by: 王国泰 <wangguotai@xiaomi.com>

--- mylibrary/src/main/java/com/example/mylibrary/leetcode/堆/p215第K个最大的元素/Temp.java
+++ mylibrary/src/main/java/com/example/mylibrary/leetcode/堆/p215第K个最大的元素/Temp.java
@@ -1,15 +1,59 @@
 package com.example.mylibrary.leetcode.堆.p215第K个最大的元素;
 
+import java.util.concurrent.CountDownLatch;
+
 public class Temp {
-    int whoCanReferMe = 0;
+    //    int whoCanReferMe = 0;
+//
+//    static class ICannotRefer {
+//
+//    }
+//
+//    class ICanRefer {
+//        private void test() {
+//            System.out.println(whoCanReferMe);
+//        }
+//    }
+    private static volatile int num = 1;
 
-    static class ICannotRefer {
+    public static void main(String[] args) {
+        Thread[] threads = new Thread[3];
+//    ReentrantLock lock =new ReentrantLock();
+        CountDownLatch latch = new CountDownLatch(6);
+        final int[] index = {1, 2, 3};
+        Object lock = new Object();
+        for (int i = 0; i < threads.length; i++) {
+            threads[i] = new Thread(new Runnable() {
+                private int id; // 1 ,  2, 3
 
-    }
+                @Override
+                public void run() {
+                    while (true) {
+                        synchronized (lock) {
+                            if (id == num % 4) {
+                                System.out.println(num++);
+                                lock.notifyAll();
+                            } else {
+                                try {
+                                    lock.wait();
+                                } catch (InterruptedException e) {
+                                    throw new RuntimeException(e);
+                                }
+                            }
+                        }
+                    }
 
-    class ICanRefer {
-        private void test() {
-            System.out.println(whoCanReferMe);
+
+//                if( index[id] == latch.getCount() % 4){
+//                    System.out.println(num++);
+//                    latch.countDown();
+//                }
+//
+                }
+            });
+            threads[i].start();
+//        threads[i].getId() = 1;
         }
     }
+
 }
---
+++ mylibrary/src/main/java/com/example/mylibrary/leetcode/每日一题/P2385感染二叉树需要的总时间.java
@@ -0,0 +1,67 @@
+package com.example.mylibrary.leetcode.每日一题;
+
+import com.example.mylibrary.leetcode.二叉树.TreeNode;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Queue;
+import java.util.Set;
+
+public class P2385感染二叉树需要的总时间 {
+
+}
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
+ * }
+ */
+class Solution {
+    public int amountOfTime(TreeNode root, int start) {
+        Map<Integer, List<Integer>> graph = new HashMap<Integer, List<Integer>>();
+        dfs(graph, root);
+        Queue<int[]> queue = new LinkedList<>();
+        queue.offer(new int[]{start, 0});
+        Set<Integer> visited = new HashSet<Integer>();
+        visited.add(start);
+        int time = 0;
+        while (!queue.isEmpty()) {
+            int[] arr = queue.poll();
+            int nodeVal = arr[0];
+            time = arr[1];
+            for (int childVal : graph.get(nodeVal)) {
+                if (visited.add(childVal)) {
+                    queue.offer(new int[]{childVal, time + 1});
+                }
+            }
+        }
+        return time;
+    }
+
+    public void dfs(Map<Integer, List<Integer>> graph, TreeNode node) {
+        graph.putIfAbsent(node.val, new ArrayList<Integer>());
+        for (TreeNode child : Arrays.asList(node.left, node.right)) {
+            if (child != null) {
+                graph.get(node.val).add(child.val);
+                graph.putIfAbsent(child.val, new ArrayList<>());
+                graph.get(child.val).add(node.val);
+                dfs(graph, child);
+            }
+        }
+    }
+}

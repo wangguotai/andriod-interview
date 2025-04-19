4f16fe7a6094d4b0b6309531942e76560c0f377c
wangguotai <965153496@qq.com>
2025-04-20 00:04:37 +0800

    feat: normal

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,10 +1,25 @@
 pluginManagement {
     repositories {
-        maven("https://maven.aliyun.com/repository/google")
-        maven("https://maven.aliyun.com/repository/public")
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+//pluginManagement {
+//    repositories {
+//        maven("https://maven.aliyun.com/repository/google")
+//        maven("https://maven.aliyun.com/repository/public")
 //        maven ("https://maven.aliyun.com/repository/central")
-        maven("https://maven.aliyun.com/repository/gradle-plugin")
-        // 国内无法使用google源，使用上面的阿里云镜像替代
+//        maven("https://maven.aliyun.com/repository/gradle-plugin")
+//        // 国内无法使用google源，使用上面的阿里云镜像替代
 //        google {
 //            content {
 //                includeGroupByRegex("com\\.android.*")
@@ -12,31 +27,31 @@ pluginManagement {
 //                includeGroupByRegex("androidx.*")
 //            }
 //        }
-        google()
+////        google()
+////        mavenCentral()
+//        gradlePluginPortal()
+//    }
+//}
+//dependencyResolutionManagement {
+//    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+//    repositories {
+//        maven ("https://maven.aliyun.com/repository/public/")
+//        maven ("https://maven.aliyun.com/repository/central")
+//        google()
+//        mavenLocal()
 //        mavenCentral()
-        gradlePluginPortal()
-    }
-}
-dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
-    repositories {
-        maven ("https://maven.aliyun.com/repository/public/")
-        maven ("https://maven.aliyun.com/repository/central")
-        google()
-        mavenLocal()
-        mavenCentral()
-    }
-}
-
-rootProject.name = "My Application"
-include(":app")
-//include(":mylibrary")
-include(":scroll-event-demo")
-include(":xshell")
-include(":xshell:login")
-include(":xshell:order")
-include(":xshell:common")
-include(":xshell:mrouter-annotations")
-include(":xshell:mrouter-compiler")
-include(":xshell:mrouter-api")
-include(":xshell:rnCommon")
+//    }
+//}
+//
+//rootProject.name = "My Application"
+//include(":app")
+////include(":mylibrary")
+//include(":scroll-event-demo")
+//include(":xshell")
+//include(":xshell:login")
+//include(":xshell:order")
+//include(":xshell:common")
+//include(":xshell:mrouter-annotations")
+//include(":xshell:mrouter-compiler")
+//include(":xshell:mrouter-api")
+//include(":xshell:rnCommon")
